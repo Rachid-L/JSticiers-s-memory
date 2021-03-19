a3318e321e09ef44f10b66a195f9a2a728d7b51e
Rachid-L <rachid-leboukh_student2021@wilder.school>
2021-03-19 13:50:22 +0100

    script.js modified

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const generalMenu = document.querySelector("nav");
-const menuBtn = document.querySelector(".dropdown-btn");
-const menu = document.querySelector(".dropdown-menu-content");
+let clicBtn = document.getElementById("play");
 
-menuBtn.addEventListener("click", function () {
-  menu.classList.toggle("hidden");
-  generalMenu.classList.toggle("hidden");
-});
+let menu = document.getElementById("barreMenu")
+
+clicBtn.addEventListener("click", () => {
+  if(getComputedStyle(menu).display != "none"){
+ 
+  }
+}
+
+
+
+
+)
