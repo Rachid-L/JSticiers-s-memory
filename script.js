be7dcdbb50e15cba4c8384e580a8a73d13662a15
Mauranne Lagneau <mauranne.lagneau@gmail.com>
2021-03-22 00:23:54 +0100

    Adding functions to make a tabgame + get image value and set the src of the img

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let clicBtn = document.getElementById("play");
 let menu = document.getElementById("barreMenu");
 
@@ -12,7 +10,6 @@ if (window.matchMedia("(max-width: 900px)").matches) {
   });
 }
 
-
 /* Fonction qui permet de cacher-rendre visible le contenu du menu déroulant 
 
 const generalMenu = document.querySelector('nav');
@@ -24,6 +21,79 @@ menuBtn.addEventListener('click', () => {
   generalMenu.classList.toggle('hidden');
 }); */
 
+// Mise en place du jeu
+
+const cardGrid = document.querySelector("#cardGrid");
+
+var arrayGame = [
+  [1,0,3,0],
+  [0,1,2,0],
+  [0,0,0,0],
+  [0,0,0,0]
+];
+
+
+function displayCard(){
+  var txt = "";
+  for (let i = 0; i<arrayGame.length; i++){
+      txt += "<div>";
+      for (let j=0; j<arrayGame[i].length; j++){
+        if (arrayGame[i][j] === 0){
+          txt +="<img class='card' src='img/logo.png'>";
+        } 
+        else{
+          txt +="<img class='card' src='" + getImage(arrayGame[i][j]) + "'>";
+        };
+      }
+      txt += "</div>";
+  }
+  cardGrid.innerHTML = txt;
+};
+
+function getImage(valeur){
+  var imgSrc = "";
+  switch(valeur){
+    case 1:
+      imgSrc += "img/elephant.png";
+      break;
+    case 2:
+      imgSrc += "img/giraffe.png";
+      break;
+    case 3:
+      imgSrc += "img/hippo.png";
+      break;
+    case 4:
+      imgSrc += "img/monkey.png";
+      break;
+    case 5:
+      imgSrc += "img/panda.png";
+      break;
+    case 6:
+      imgSrc += "img/parrot.png";
+      break;
+    case 7:
+      imgSrc += "img/penguin.png";
+      break;
+    case 8:
+      imgSrc += "img/pig.png";
+      break;
+    case 9:
+      imgSrc += "img/rabbit.png";
+      break;
+    case 10:
+      imgSrc += "img/snake.png";
+      break;
+  }
+  return imgSrc;
+};
+
+getImage();
+displayCard();
+
+
+/*let card = document.createElement("div");
+card.classList.add("card");
+cardGrid.appendChild(card);*/
 
 // Application d'un addeventlistener pour flip over les cartes au clic
 
