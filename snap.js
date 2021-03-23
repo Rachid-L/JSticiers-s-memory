7c3dcabeac6f1c651aa76d72a57dfd3d1063ff11
Rachid-L <rachid-leboukh_student2021@wilder.school>
2021-03-23 16:17:01 +0100

    Debug after adding the function for the menu to disappear

---
+++ snap.js
@@ -0,0 +1,12 @@
+// Mise en place de la fonctionnalité d'effacement du menu latéral lorsque le joueur appuie sur "Play" 
+
+let clicBtn = document.getElementById("play");
+let menu = document.getElementById("barreMenu");
+
+
+
+clicBtn.addEventListener("click", () => {
+    if (getComputedStyle(menu).display != "none") {
+    menu.style.display = "none";
+    }
+});
